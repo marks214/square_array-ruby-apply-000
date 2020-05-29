3897160a0e4c7133f9e662c6e73df51b31f87b83
Aimee Oz <aimee.oz.x@gmail.com>
2020-05-29 17:52:08 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   # your code here
   counter = 0
   square_array = []
-  array.each do |square|
-    squared = square.to_i ** 2
+  array.each do |numbers|
+    squared = numbers.to_i ** 2
     square_array[counter] << squared
     counter += 1
   end
