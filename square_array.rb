3e8bf1c4c23e39743c06cf9e23e8ee42e2a25df3
Aimee Oz <aimee.oz.x@gmail.com>
2020-05-29 17:49:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
   square_array = []
   array.each do |square|
-    square_array[counter] << square ** 2 
+    square_array[counter] << square ** 2
     counter += 1
   end
 end
