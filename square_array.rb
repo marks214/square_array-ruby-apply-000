5c59447952f6d26425c29e72c8f7fe82ccc77470
Aimee Oz <aimee.oz.x@gmail.com>
2020-05-29 18:06:10 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+    # your code here
+    squared_array = []
+    array.each do |numbers|
+      squared = numbers.to_i ** 2
+      squared_array.push(squared)
+    end
+    return squared_array
 end
